d8eb64abf6bdbfed92b5bf2e4c26c6a8277b41ae
Maureen Garcia <cmaureen1908@gmail.com>
2025-03-19 12:44:19 -0500

    created the layout of search on pictures component

--- src/Pages/Pictures.jsx
+++ src/Pages/Pictures.jsx
@@ -1,7 +1,15 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import SkeletonPicture from "../Components/SkeletonPicture/SkeletonPicture";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Pictures = () => {
   const cities = [
@@ -26,43 +34,70 @@ const Pictures = () => {
   const API_KEY = import.meta.env.VITE_PEXELS_API_KEY;
 
   useEffect(() => {
-    const fetchPhotos = async () => {
-      const allPhotos = [];
-
-      for (let city of cities) {
-        try {
-          const res = await axios.get(`https://api.pexels.com/v1/search`, {
-            headers: {
-              Authorization: API_KEY,
-            },
-            params: {
-              query: city,
-              per_page: 1,
-            },
-          });
-          if (res.data.photos.length > 0) {
-            allPhotos.push({
-              city,
-              photoUrl: res.data.photos[0].src.large,
-            });
-          }
-        } catch (err) {
-          console.error(err);
-        }
-      }
-
-      setPhotosData(allPhotos);
-      setLoading(false);
-    };
-
     fetchPhotos();
   }, []);
 
+  const fetchPhotos = async () => {
+    const allPhotos = [];
+
+    for (let city of cities) {
+      try {
+        const res = await axios.get(`https://api.pexels.com/v1/search`, {
+          headers: {
+            Authorization: API_KEY,
+          },
+          params: {
+            query: city,
+            per_page: 1,
+          },
+        });
+        if (res.data.photos.length > 0) {
+          allPhotos.push({
+            city,
+            photoUrl: res.data.photos[0].src.large,
+          });
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    setPhotosData(allPhotos);
+    setLoading(false);
+  };
   return (
     <Box sx={{ width: "80%", margin: "0 auto", paddingTop: "80px" }}>
       <Typography variant="h2" sx={{ marginY: "20px" }}>
         Photo Gallery
       </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          marginY: "20px",
+        }}
+      >
+        <Paper
+          component="form"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: 400,
+            p: "2px 20px",
+            mx: "auto",
+          }}
+        >
+          <InputBase sx={{ width: "90%" }} placeholder="Search pictures" />
+          <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        <Typography variant="h6">
+          You can search pictures of different countries
+        </Typography>
+      </Box>
+
       <Box display="flex" flexWrap="wrap" justifyContent="center" gap={2}>
         {loading ? (
           <SkeletonPicture />
