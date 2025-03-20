942ae119c00cba801d8da01b041ffb4724eaade8
Maureen Garcia <cmaureen1908@gmail.com>
2025-03-20 11:25:36 -0500

    added paper to search on infoCountries component

--- src/Pages/InfoCountries.jsx
+++ src/Pages/InfoCountries.jsx
@@ -1,14 +1,33 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  IconButton,
+  InputBase,
+  MenuItem,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import CardCountries from "../Components/CardCountries/CardCountries";
 import SkeletonInfoCountries from "../Components/SkeletonInfoCountries/SkeletonInfoCountries";
+import SearchIcon from "@mui/icons-material/Search";
 
 const InfoCountries = () => {
   const [countriesData, setCountriesData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [countrieSearch, setCountrieSearch] = useState("");
   const [searchedCountrie, setSearchedCountrie] = useState({});
+  const typeSearch = [
+    { value: "lang", label: "Languages" },
+    { value: "name", label: "Name of the country" },
+    { value: "currency", label: "Currency" },
+    {
+      value: "capital",
+      label: "Capital City",
+    },
+  ];
 
   useEffect(() => {
     fetchCountries();
@@ -41,6 +60,61 @@ const InfoCountries = () => {
       <Typography variant="h2" sx={{ marginBottom: "30px" }}>
         General Information about countries
       </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          textAlign: "center",
+          marginY: "20px",
+        }}
+      >
+        <Typography variant="h6">
+          You can search for information on countries of interest to you.
+        </Typography>
+        <Typography variant="xs">
+          Choose an option for the search, and input the name to that option.
+        </Typography>
+        <Paper
+          component="form"
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: 400,
+            p: "10px 20px",
+            mx: "auto",
+            my: "20px",
+            gap: 2,
+          }}
+        >
+          <TextField
+            id="searcheOptions"
+            select
+            label="Options to search"
+            defaultValue="lang"
+            helperText="Choose an option and "
+            sx={{ width: "100%" }}
+          >
+            {typeSearch.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            required
+            id="outlined-required"
+            label="Describe for search"
+            helperText="Type for search"
+            placeholder="Example: spanish"
+            sx={{ width: "100%" }}
+          />
+          <Button endIcon={<SearchIcon />} variant="outlined">
+            Search
+          </Button>
+        </Paper>
+      </Box>
 
       {countriesData.length > 0 && (
         <Box
