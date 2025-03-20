1bc5d49567a7912191714ce68d17f5dbf3cef9a6
Maureen Garcia <cmaureen1908@gmail.com>
2025-03-20 16:22:59 -0500

    avance on picture componente responsive

--- src/Pages/Pictures.jsx
+++ src/Pages/Pictures.jsx
@@ -1,10 +1,20 @@
-import { Box, IconButton, InputBase, Paper, Typography } from "@mui/material";
+import {
+  Box,
+  IconButton,
+  InputBase,
+  Paper,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import SkeletonPicture from "../Components/SkeletonPicture/SkeletonPicture";
 import SearchIcon from "@mui/icons-material/Search";
 
 const Pictures = () => {
+  const themeQuery = useTheme();
+  const isMobile = useMediaQuery(themeQuery.breakpoints.down("sm"));
   const cities = [
     "Prague",
     "Rome",
@@ -97,97 +107,197 @@ const Pictures = () => {
   };
 
   return (
-    <Box sx={{ width: "80%", margin: "0 auto", paddingTop: "80px" }}>
-      <Typography variant="h2" sx={{ marginY: "20px" }}>
-        Photo Gallery
-      </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          textAlign: "center",
-          marginY: "20px",
-        }}
-      >
-        <Paper
-          component="form"
-          onSubmit={handleSubmit}
-          sx={{
-            display: "flex",
-            alignItems: "center",
-            width: 400,
-            p: "2px 20px",
-            mx: "auto",
-          }}
-        >
-          <InputBase
-            sx={{ width: "90%" }}
-            placeholder="Search pictures"
-            value={placeSearch}
-            onChange={handleInputChange}
-          />
-          <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-            <SearchIcon />
-          </IconButton>
-        </Paper>
-        <Typography variant="h6">
-          You can search pictures of different countries or cities.
-        </Typography>
-        <Typography variant="xs">
-          In results you will see photos of trips to in that place.
-        </Typography>
-      </Box>
+    <>
+      {isMobile ? (
+        <Box sx={{ width: "80%", margin: "0 auto", paddingTop: "80px" }}>
+          <Typography variant="h2" sx={{ marginY: "20px" }}>
+            Photo Gallery
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              marginY: "20px",
+            }}
+          >
+            <Paper
+              component="form"
+              onSubmit={handleSubmit}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                width: " 80%",
+                p: "2px 20px",
+                mx: "auto",
+              }}
+            >
+              <InputBase
+                sx={{ width: "90%" }}
+                placeholder="Search pictures"
+                value={placeSearch}
+                onChange={handleInputChange}
+              />
+              <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+            <Typography variant="sm" sx={{ marginTop: "10px" }}>
+              You can search pictures of different countries or cities.
+            </Typography>
+            <Typography variant="xs">
+              In results you will see photos of trips to in that place.
+            </Typography>
+          </Box>
+          {allSearchedPlace.length > 0 && (
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              gap={2}
+              mt={4}
+            >
+              {loading ? (
+                <SkeletonPicture />
+              ) : (
+                allSearchedPlace.map((photo, index) => (
+                  <Box key={index} p={1}>
+                    <img
+                      src={photo.photoUrl}
+                      alt={photo.placeSearch}
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                      }}
+                    />
+                  </Box>
+                ))
+              )}
+            </Box>
+          )}
 
-      {allSearchedPlace.length > 0 && (
-        <Box
-          display="flex"
-          flexWrap="wrap"
-          justifyContent="center"
-          gap={2}
-          mt={4}
-        >
-          {loading ? (
-            <SkeletonPicture />
+          {allSearchedPlace.length > 0 ? (
+            <Box />
           ) : (
-            allSearchedPlace.map((photo, index) => (
-              <Box key={index} p={1}>
-                <img
-                  src={photo.photoUrl}
-                  alt={photo.placeSearch}
-                  style={{
-                    width: "auto",
-                    height: "300px",
-                  }}
-                />
-              </Box>
-            ))
+            <Box
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              gap={2}
+            >
+              {loading ? (
+                <SkeletonPicture />
+              ) : (
+                photosData.map((photo, index) => (
+                  <Box key={index} p={1}>
+                    <img
+                      src={photo.photoUrl}
+                      alt={photo.city}
+                      style={{
+                        width: "100%",
+                        height: "auto",
+                      }}
+                    />
+                  </Box>
+                ))
+              )}
+            </Box>
           )}
         </Box>
-      )}
-
-      {allSearchedPlace.length > 0 ? (
-        <Box />
       ) : (
-        <Box display="flex" flexWrap="wrap" justifyContent="center" gap={2}>
-          {loading ? (
-            <SkeletonPicture />
+        <Box sx={{ width: "80%", margin: "0 auto", paddingTop: "80px" }}>
+          <Typography variant="h2" sx={{ marginY: "20px" }}>
+            Photo Gallery
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              textAlign: "center",
+              marginY: "20px",
+            }}
+          >
+            <Paper
+              component="form"
+              onSubmit={handleSubmit}
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                width: 400,
+                p: "2px 20px",
+                mx: "auto",
+              }}
+            >
+              <InputBase
+                sx={{ width: "90%" }}
+                placeholder="Search pictures"
+                value={placeSearch}
+                onChange={handleInputChange}
+              />
+              <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+            <Typography variant="h6">
+              You can search pictures of different countries or cities.
+            </Typography>
+            <Typography variant="xs">
+              In results you will see photos of trips to in that place.
+            </Typography>
+          </Box>
+
+          {allSearchedPlace.length > 0 && (
+            <Box
+              display="flex"
+              flexWrap="wrap"
+              justifyContent="center"
+              gap={2}
+              mt={4}
+            >
+              {loading ? (
+                <SkeletonPicture />
+              ) : (
+                allSearchedPlace.map((photo, index) => (
+                  <Box key={index} p={1}>
+                    <img
+                      src={photo.photoUrl}
+                      alt={photo.placeSearch}
+                      style={{
+                        width: "auto",
+                        height: "300px",
+                      }}
+                    />
+                  </Box>
+                ))
+              )}
+            </Box>
+          )}
+
+          {allSearchedPlace.length > 0 ? (
+            <Box />
           ) : (
-            photosData.map((photo, index) => (
-              <Box key={index} p={1}>
-                <img
-                  src={photo.photoUrl}
-                  alt={photo.city}
-                  style={{
-                    width: "auto",
-                    height: "300px",
-                  }}
-                />
-              </Box>
-            ))
+            <Box display="flex" flexWrap="wrap" justifyContent="center" gap={2}>
+              {loading ? (
+                <SkeletonPicture />
+              ) : (
+                photosData.map((photo, index) => (
+                  <Box key={index} p={1}>
+                    <img
+                      src={photo.photoUrl}
+                      alt={photo.city}
+                      style={{
+                        width: "auto",
+                        height: "300px",
+                      }}
+                    />
+                  </Box>
+                ))
+              )}
+            </Box>
           )}
         </Box>
       )}
-    </Box>
+    </>
   );
 };
 
